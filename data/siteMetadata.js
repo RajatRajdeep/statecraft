d374dd716f5e09567720b502d35443fd52812df7
Rajat Rajdeep <rajatrajdeep@outlook.com>
2026-05-25 00:21:51 +0530

    add favicon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -44,9 +44,9 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+    googleAnalytics: {
+      googleAnalyticsId: 'G-R3C39CMMEP',
+    },
   },
   newsletter: {},
   comments: {
